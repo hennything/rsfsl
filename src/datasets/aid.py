c8091892cc31303c8a982b769dd3c1291d69d8f3
hennything <kok.gareth@gmail.com>
2024-08-29 13:20:47 +0200

    Create aid.py

---
+++ src/datasets/aid.py
@@ -0,0 +1,21 @@
+from datasets.dataset_utils import load_dataset_from_pkl, load_dataset_from_from_folder 
+from datasets.dataset_template import ColorDatasetInMemory, ColorDatasetOnDisk
+import os
+
+def get_Aid(args_per_set, setnames=["train", "val", "test"]):
+    """
+    Returns aid datasets.
+    """
+    datasets = {}
+    for setname in setnames:
+        args = args_per_set[setname]
+            
+        data_path = os.path.abspath(args.data_path)
+        
+        filepath = os.path.join(data_path, "aid", "aid-cache-{0}.pkl".format(setname))
+        data = load_dataset_from_pkl(filepath)
+        dataset_class = ColorDatasetInMemory
+            
+        datasets[setname] = [data['image_data'], data['class_dict'], args, dataset_class]
+        
+    return datasets
