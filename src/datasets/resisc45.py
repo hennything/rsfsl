8e692c5506f8a4b5b1cf8267523239c9386051b8
hennything <kok.gareth@gmail.com>
2024-08-30 09:02:58 +0200

    Create resisc45.py

---
+++ src/datasets/resisc45.py
@@ -0,0 +1,19 @@
+from datasets.dataset_utils import load_dataset_from_pkl, load_dataset_from_from_folder 
+from datasets.dataset_template import ColorDatasetInMemory, ColorDatasetOnDisk
+import os
+
+def get_Resisc45(args_per_set, setnames=["train", "val", "test"]):
+    """
+    Returns resisc45 datasets.
+    """
+    datasets = {}
+    for setname in setnames:
+        args = args_per_set[setname]
+        data_path = os.path.abspath(args.data_path)
+        filepath = os.path.join(data_path, "resisc45", "resisc45-cache-{0}.pkl".format(setname))
+        data = load_dataset_from_pkl(filepath)
+        dataset_class = ColorDatasetInMemory
+            
+        datasets[setname] = [data['image_data'], data['class_dict'], args, dataset_class]
+        
+    return datasets
