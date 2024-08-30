e4ef19f4f459fd1166799028dfcbfab249f9d579
hennything <kok.gareth@gmail.com>
2024-08-30 17:51:48 +0200

    Update batch_simple.py

--- src/tasks/batch_simple.py
+++ src/tasks/batch_simple.py
@@ -1,25 +1,34 @@
 import argparse
+import numpy as np
+from tasks.task_template import TaskTemplate
+import math
 
-class TaskTemplate():
-    
+class SimpleBatchTask(TaskTemplate):
+        
     @staticmethod
     def get_parser(parser=None):
         if parser is None: parser=argparse.ArgumentParser()
+        parser.add_argument('--batch_size', type=int, default=32)
         return parser
     
     @staticmethod
     def get_output_dim(args, dataset):
-        raise NotImplementedError
-    
+        return dataset.get_num_classes()
+        
     def __init__(self, dataset, args, class_seed, sample_seed):
-        self.dataset = dataset
-        self.args = args
-        self.class_seed = class_seed
-        self.sample_seed = sample_seed
+        super().__init__(dataset, args, class_seed, sample_seed)
+        self.batch_size = args.batch_size
     
     def __len__(self):
-        raise NotImplementedError
+        return 1
     
     def __iter__(self):
-        raise NotImplementedError
+        rng = np.random.RandomState(self.sample_seed)
+        idx = rng.choice(np.arange(len(self.dataset)), self.batch_size, replace=False)
+        seeds = rng.randint(0, 999999, (self.batch_size,))
+        labels = [(self.dataset.class_name_to_id[self.dataset.inv_class_dict[i]], i) for i, seed in zip(idx, seeds) ]
+        support_set = (idx, labels)
+        target_set = ([], [])
+        yield (support_set, target_set)
+        
             
